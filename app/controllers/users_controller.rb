0c1a209ac8fe5211a1406350e2ba34e22f12db31
daisuke-shimura <daisuke.s0707@icloud.com>
2026-04-12 03:23:24 +0900

    ユーザ削除機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path, notice: 'User deleted successfully.'
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :gender, :birthday, :experience)
