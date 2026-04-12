6fae85e001ee3e19b7e9127338b86ed857743d4e
daisuke-shimura <daisuke.s0707@icloud.com>
2026-04-12 15:01:33 +0900

    登録画面のUI工場

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'User created successfully.'
+      redirect_to records_path, notice: 'User created successfully.'
     else
       render :new
     end
