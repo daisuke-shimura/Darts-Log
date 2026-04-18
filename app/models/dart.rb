b68e3d8e7ff73f24526485735f9f96fecfbb7e08
daisuke-shimura <daisuke.s0707@icloud.com>
2026-04-18 11:52:25 +0900

    segmentカラム ラベルメソッドを作成

--- app/models/dart.rb
+++ app/models/dart.rb
@@ -2,10 +2,13 @@ class Dart < ApplicationRecord
   before_validation :normalize_absolute_0
   belongs_to :round
 
-  enum segument: { bull: 0 }
   enum multiplier: { out: 0, single: 1, double: 2, triple: 3 }
 
   def normalize_absolute_0
     self.absolute_0 = absolute_0.to_d.round(2) if absolute_0.present?
+  end
+
+  def segment_label
+    self.segment == 50 ? "BULL" : self.segment.to_s
   end
 end
