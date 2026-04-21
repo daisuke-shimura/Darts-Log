d8500aa257de7678223262cc9e471db023768fbc
daisuke-shimura <daisuke.s0707@icloud.com>
2026-04-21 15:40:00 +0900

    結果出力画面の調整

--- app/javascript/controllers/hole_controller.js
+++ app/javascript/controllers/hole_controller.js
@@ -38,7 +38,8 @@ export default class extends Controller {
 
       this.selected.forEach((p, index) => {
         if (this.outputTargets[index]) {
-          this.outputTargets[index].textContent = `${p.name} ${p.value}点, (r, θ) = (${p.absolute_r}, ${p.absolute_0}), (r, n) = (${p.r}, ${p.n})`;
+          const html = `${p.name} ${p.value}点<br>(r, θ) = (${p.absolute_r}, ${p.absolute_0})<br>(r, n) = (${p.r}, ${p.n})`;
+          this.outputTargets[index].innerHTML = html;
         }
       });
   }
