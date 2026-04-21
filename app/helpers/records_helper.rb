bcf09c4e70c0306ed9331ad7bcd0eda4c4b487d1
daisuke-shimura <daisuke.s0707@icloud.com>
2026-04-21 17:32:27 +0900

    θ計算をヘルパー化

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,11 @@
 module RecordsHelper
+  def bull_top_standard(index_number, division_number)
+    (90 - (index_number - 1) * (360.0 / division_number) + 360) % 360
+  end
+
+  def bull_zero_standard(index_number, division_number, zero_number)
+    #(((zero_number - 1) - (index_number - 1) + division_number) % division_number) * (360.0 / division_number)
+    ((zero_number - index_number + division_number) % division_number) * (360.0 / division_number)
+  end
+
 end
