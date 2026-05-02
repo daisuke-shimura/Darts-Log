3447ed7e2ad41d70dc8e144cf120b6858db6cadd
daisuke-shimura <daisuke.s0707@icloud.com>
2026-05-02 22:23:01 +0900

    ゼロワンのスコア表示

--- app/controllers/games/crickets_controller.rb
+++ app/controllers/games/crickets_controller.rb
@@ -5,5 +5,8 @@ class Games::CricketsController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    @defalut_target = "bull"
+    @defalut_target_name = "BULL"
+    @segment_index = [20,1,18,4,13,6,10,15,2,17,3,19,7,16,8,11,14,9,12,5]
   end
 end
