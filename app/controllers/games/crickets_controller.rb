e98502a1ce7ae2ff08cb60f4e5e7c3df94ec39d0
daisuke-shimura <daisuke.s0707@icloud.com>
2026-05-05 09:45:36 +0900

    パスの修正

--- app/controllers/games/crickets_controller.rb
+++ app/controllers/games/crickets_controller.rb
@@ -4,7 +4,7 @@ class Games::CricketsController < ApplicationController
   end
 
   def show
-    @game = Game.find(params[:id])
+    @game = Game.find(params[:game_id])
     @defalut_target = "bull"
     @defalut_target_name = "BULL"
     @segment_index = [20,1,18,4,13,6,10,15,2,17,3,19,7,16,8,11,14,9,12,5]
