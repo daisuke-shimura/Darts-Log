332ab34953738cf81d9b3ee4416cbba91e81c288
daisuke-shimura <daisuke.s0707@icloud.com>
2026-05-07 22:27:34 +0900

    カラムを追加してdbをリセットした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to records_path, notice: 'User created successfully.'
+      redirect_to root_path, notice: '登録が完了しました'
     else
       render :new
     end
