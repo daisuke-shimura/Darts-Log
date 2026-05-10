f0ee54e6867da6bd7af126605cda2c1f8cae84dd
daisuke-shimura <daisuke.s0707@icloud.com>
2026-05-10 10:49:45 +0900

    日本時間に変更

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module DartsLog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Asia/Tokyo"
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
   end
 end
