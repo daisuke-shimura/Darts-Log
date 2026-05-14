f7e4c5eda43d697aa9987476a894bbf98f21e1d8
daisuke-shimura <daisuke.s0707@icloud.com>
2026-05-15 00:19:11 +0900

    色々と追加

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,18 @@
 class LogsController < ApplicationController
   def index
     @darts_data = Dart.all
+
+    #ダミーデータ
+    dummy_points = [
+      # ブル付近
+      { x: 500, y: 500, value: 1 }, { x: 510, y: 490, value: 1 },
+      { x: 480, y: 520, value: 1 }, { x: 505, y: 515, value: 1 },
+      { x: 495, y: 495, value: 1 }, { x: 520, y: 500, value: 1 },
+      # 20点付近
+      { x: 500, y: 150, value: 1 }, { x: 490, y: 160, value: 1 },
+      { x: 510, y: 140, value: 1 }, { x: 495, y: 130, value: 1 },
+      { x: 505, y: 155, value: 1 }
+    ]
+    @heatmap_data = dummy_points
   end
 end
