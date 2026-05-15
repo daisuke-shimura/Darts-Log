c52ef404b60f4a8edf8c6e6590a9511096a30d57
daisuke-shimura <daisuke.s0707@icloud.com>
2026-05-15 18:57:55 +0900

    絞り込み

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,33 @@
 class LogsController < ApplicationController
   def index
-    @darts_data = Dart.all
+    @Record_darts = Dart.where.not(record_round_id: nil)
+    @Record_join = Dart.all.joins(:record_round)
+    @Game_darts = Dart.where.not(game_round_id: nil)
+    @Game_all = Dart.all
+
+    game_data = Dart.none
+    record_data = Dart.none
+  
+    if params[:kinds].present?
+      game_data = Dart
+        .joins(game_round: :game)
+        .where(
+          games: {
+            kind: params[:kinds]
+          }
+        )
+    end
+  
+    if params[:record] == "1"
+      record_data = Dart.joins(:record_round)
+    end
+  
+    if params[:kinds].blank? && params[:record].blank?
+      @darts_data = Dart.all
+    elsif params[:kinds].present? && params[:record] == "1"
+      @darts_data = game_data.to_a | record_data.to_a
+    else
+      @darts_data = game_data.presence || record_data
+    end
   end
 end
